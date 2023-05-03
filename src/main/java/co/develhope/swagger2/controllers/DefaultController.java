47a6e2bca57cbd2420e26b67a6c27c7bfdbcf628
DavideDiFranco95 <Davide.difranco_@outlook.it>
2023-05-03 16:53:26 +0200

    Spring Boot Swagger02

--- src/main/java/co/develhope/swagger2/controllers/DefaultController.java
+++ src/main/java/co/develhope/swagger2/controllers/DefaultController.java
@@ -1,9 +1,15 @@
 package co.develhope.swagger2.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/default")
+@RequestMapping("")
 public class DefaultController {
+
+    @GetMapping
+    public String helloMessage(){
+        return "Hello and Welcome!";
+    }
 }
